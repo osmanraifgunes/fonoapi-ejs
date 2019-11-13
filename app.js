2850bcbf9ed2c2d2ffbefca69f5a1009e227cafe
Osman Raif Güneş <osmanraifgunes@gmail.com>
2019-11-13 20:20:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'))
+var fonoapi = require('./phone-app');
+fonoapi.token = 'your_token';
+
+
+app.get("/", function (req, res) {
+    return fonoapi.getDevices(function (queryString, data) {
+        res.render("home.ejs", { tels: JSON.stringify(data)});
+    },req.query.device, req.query.brand);
+});
+
+app.listen(3000, function () {
+    console.log("server is listening!!!");
+});
